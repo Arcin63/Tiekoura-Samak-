cfa50b0838962d9c8bbad500a0264ac0501defe0
Arcin63 <hackingpiratage6@gmail.com>
2026-09-09 23:48:45 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Numéro WhatsApp au format international, SANS "+" ni espaces
+  // Exemple pour le Mali : 22377889900
+  phoneNumber: 'METTEZ_VOTRE_NUMERO_ICI', // <-- REMPLACE ÇA PAR TON VRAI NUMÉRO
+
+  // Code de jumelage : 8 caractères MAX. Lettres et chiffres seulement
+  pairingCode: 'TKRSMKBU', // 8 caractères. Parfait
+
+  // Préfixe utilisé pour toutes les commandes
+  prefix: '!', // Tu peux mettre ! ou . ou /
+
+  // Nom affiché du bot
+  botName: 'Tiekoura Samaké bot',
+
+  // Ligne de crédit affichée dans le menu et les infos
+  credit: 'Bot créé par le savant Arcin le genie ✨',
+};
